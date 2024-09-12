08218bc2ff1abea1ee9d1179a7b2681c729dc8b9
booren <yasin.arran@gmail.com>
2024-09-12 22:20:03 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,16 @@ const App = () => {
   const { toggleTheme, theme } = useTheme()
 
   return (
-    <div>
-      <button className='button button--flex' onClick={() => toggleTheme()}>
-        {theme === "light" ? "light mode" : "dark mode"} <i className="button__icon"><FaAirbnb /></i>
-      </button>
+    <div className='main'>
+      <div className="main__container">
+        <h1 className="main__title ">pres to change the theme</h1>
+
+        <button className='main__button button button--flex' onClick={() => toggleTheme()}>
+          {theme === "light" ? "light mode" : "dark mode"}<i className="button__icon"><FaAirbnb /></i>
+          <p className='main__tap'>tap</p>
+        </button>
+
+      </div>
     </div>
   )
 }
