833dd4e83c39921b6fb09d63d1ede979c9a59960
booren <yasin.arran@gmail.com>
2024-09-16 18:43:38 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import Auth from './pages/login/Auth'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Register from './pages/register/Register'
-import Main from './pages/main/Main'
+import React, { useState } from 'react'
+import "./App.css"
+import { useTheme } from './common/ColorTheme'
+import { FaAirbnb } from "react-icons/fa";
+
 
 const App = () => {
+  const { toggleTheme, theme } = useTheme();
+  const [snackbar, setSnackbar] = useState(true)
 
+  const toggleSnackbar = () => {
+    setSnackbar(!snackbar);
+    setTimeout(() => {
+      setSnackbar(snackbar)
+    }, 2000);
+  }
 
   return (
+    <div className='main'>
+      <div className="main__container ">
+        <h1 className="main__title ">pres to change the theme</h1>
 
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/login' element={<Auth />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+        <button className='main__button button button--flex' onClick={() => { toggleTheme(); toggleSnackbar() }}>
+          {theme === "light" ? "light mode" : "dark mode"}<i className="main__button-icon"><FaAirbnb className='react-icons' /></i>
+          <p className='main__tap'>tap</p>
+        </button>
 
+      </div>
+      <p className={`main__button-subtitle ${snackbar ? ' ' : 'show-button-subtitle'}`}>mode has been changed</p>
+    </div>
   )
 }
 
